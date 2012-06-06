6f1a8c7dd782701bee14502a983b84a17a87af75
Aaron Leung <aaron.leung@moovweb.com>
2012-06-06 14:55:36 -0700

    Sample driver file. To build it, move it out of this directory and do a `go build gass.go`

--- gass.go
+++ gass.go
@@ -2,17 +2,22 @@ package main
 
 import (
   "os"
-  // "fmt"
+  "fmt"
   sass "gosass"
 )
 
 func main() {
   args := os.Args
 
+  if len(args) < 2 {
+    fmt.Println("Usage: gass [INPUT FILE]")
+    os.Exit(1)
+  }
+
   ctx := sass.FileContext {
     Options: sass.Options {
       OutputStyle: sass.NESTED_STYLE,
-      IncludePaths: nil,
+      IncludePaths: make([]string, 0),
     },
     InputPath:    args[1],
     OutputString: "",
@@ -22,4 +27,13 @@ func main() {
 
   sass.CompileFile(&ctx)
 
+  if ctx.ErrorStatus != 0 {
+    if ctx.ErrorMessage != "" {
+      fmt.Print(ctx.ErrorMessage)
+    } else {
+      fmt.Println("An error occured; no error message available.")
+    }
+  } else {
+    fmt.Print(ctx.OutputString)
+  }
 }
