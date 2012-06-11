b90522b940164f217a36996a03fbde9187293ac9
Aaron Leung <aaron.leung@moovweb.com>
2012-06-11 11:59:05 -0700

    Commenting out the sample contents so that everything else can compile.

--- gass.go
+++ gass.go
@@ -1,39 +1,39 @@
-package main
+// package main
 
-import (
-  "os"
-  "fmt"
-  sass "gosass"
-)
+// import (
+//   "os"
+//   "fmt"
+//   sass "gosass"
+// )
 
-func main() {
-  args := os.Args
+// func main() {
+//   args := os.Args
 
-  if len(args) < 2 {
-    fmt.Println("Usage: gass [INPUT FILE]")
-    os.Exit(1)
-  }
+//   if len(args) < 2 {
+//     fmt.Println("Usage: gass [INPUT FILE]")
+//     os.Exit(1)
+//   }
 
-  ctx := sass.FileContext {
-    Options: sass.Options {
-      OutputStyle: sass.NESTED_STYLE,
-      IncludePaths: make([]string, 0),
-    },
-    InputPath:    args[1],
-    OutputString: "",
-    ErrorStatus:  0,
-    ErrorMessage: "",
-  }
+//   ctx := sass.FileContext {
+//     Options: sass.Options {
+//       OutputStyle: sass.NESTED_STYLE,
+//       IncludePaths: make([]string, 0),
+//     },
+//     InputPath:    args[1],
+//     OutputString: "",
+//     ErrorStatus:  0,
+//     ErrorMessage: "",
+//   }
 
-  sass.CompileFile(&ctx)
+//   sass.CompileFile(&ctx)
 
-  if ctx.ErrorStatus != 0 {
-    if ctx.ErrorMessage != "" {
-      fmt.Print(ctx.ErrorMessage)
-    } else {
-      fmt.Println("An error occured; no error message available.")
-    }
-  } else {
-    fmt.Print(ctx.OutputString)
-  }
-}
+//   if ctx.ErrorStatus != 0 {
+//     if ctx.ErrorMessage != "" {
+//       fmt.Print(ctx.ErrorMessage)
+//     } else {
+//       fmt.Println("An error occured; no error message available.")
+//     }
+//   } else {
+//     fmt.Print(ctx.OutputString)
+//   }
+// }
