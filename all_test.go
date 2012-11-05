93168537b1517b520669d8d218666a3783f3a8fd
Manoj Dayaram <noj@moovweb.com>
2012-11-05 11:05:18 -0800

    go fmt

--- all_test.go
+++ all_test.go
@@ -1,75 +1,76 @@
 package gosass
 
 import (
-  "runtime"
-  "testing"
-  "io/ioutil"
-  "fmt"
+	"fmt"
+	"io/ioutil"
+	"runtime"
+	"testing"
 )
 
 func runParallel(testFunc func(chan bool), concurrency int) {
-  runtime.GOMAXPROCS(4)
-  done := make(chan bool, concurrency)
-  for i := 0; i < concurrency; i++ {
-    go testFunc(done)
-  }
-  for i := 0; i < concurrency; i++ {
-    <-done
-    <-done
-  }
-  runtime.GOMAXPROCS(1)
+	runtime.GOMAXPROCS(4)
+	done := make(chan bool, concurrency)
+	for i := 0; i < concurrency; i++ {
+		go testFunc(done)
+	}
+	for i := 0; i < concurrency; i++ {
+		<-done
+		<-done
+	}
+	runtime.GOMAXPROCS(1)
 }
 
 const numConcurrentRuns = 200
+
 // const testFileName1     = "test1.scss"
 // const testFileName2     = "test2.scss"
 // const desiredOutput     = "div {\n  color: black; }\n  div span {\n    color: blue; }\n"
 
 func compileTest(t *testing.T, fileName string) (result string) {
 
-  ctx := FileContext {
-    Options: Options {
-      OutputStyle: NESTED_STYLE,
-      IncludePaths: make([]string, 0),
-    },
-    InputPath:    fileName,
-    OutputString: "",
-    ErrorStatus:  0,
-    ErrorMessage: "",
-  }
+	ctx := FileContext{
+		Options: Options{
+			OutputStyle:  NESTED_STYLE,
+			IncludePaths: make([]string, 0),
+		},
+		InputPath:    fileName,
+		OutputString: "",
+		ErrorStatus:  0,
+		ErrorMessage: "",
+	}
 
-  CompileFile(&ctx)
+	CompileFile(&ctx)
 
-  if ctx.ErrorStatus != 0 {
-    if ctx.ErrorMessage != "" {
-      t.Error("ERROR: ", ctx.ErrorMessage)
-    } else {
-      t.Error("UNKNOWN ERROR")
-    }
-  } else {
-    result = ctx.OutputString
-  }
+	if ctx.ErrorStatus != 0 {
+		if ctx.ErrorMessage != "" {
+			t.Error("ERROR: ", ctx.ErrorMessage)
+		} else {
+			t.Error("UNKNOWN ERROR")
+		}
+	} else {
+		result = ctx.OutputString
+	}
 
-  return result
+	return result
 }
 
 const numTests = 3 // TO DO: read the test dir and set this dynamically
 
 func TestConcurrent(t *testing.T) {
-  testFunc := func(done chan bool) {
-    done <- false
-    for i := 1; i <= numTests; i++ {
-      inputFile := fmt.Sprintf("test/test%d.scss", i)
-      result := compileTest(t, inputFile)
-      desiredOutput, err := ioutil.ReadFile(fmt.Sprintf("test/test%d.css", i))
-      if err != nil {
-        t.Error(fmt.Sprintf("ERROR: couldn't read test/test%d.css", i))
-      }
-      if result != string(desiredOutput) {
-        t.Error("ERROR: incorrect output")
-      }
-    }
-    done <- true
-  }
-  runParallel(testFunc, numConcurrentRuns)
+	testFunc := func(done chan bool) {
+		done <- false
+		for i := 1; i <= numTests; i++ {
+			inputFile := fmt.Sprintf("test/test%d.scss", i)
+			result := compileTest(t, inputFile)
+			desiredOutput, err := ioutil.ReadFile(fmt.Sprintf("test/test%d.css", i))
+			if err != nil {
+				t.Error(fmt.Sprintf("ERROR: couldn't read test/test%d.css", i))
+			}
+			if result != string(desiredOutput) {
+				t.Error("ERROR: incorrect output")
+			}
+		}
+		done <- true
+	}
+	runParallel(testFunc, numConcurrentRuns)
 }
--- gosass.go
+++ gosass.go
@@ -9,49 +9,49 @@ package gosass
 */
 import "C"
 import (
-  "strings"
-  "unsafe"
+	"strings"
+	"unsafe"
 )
 
 type Options struct {
-  OutputStyle  int
-  IncludePaths []string
-  ImagePath    string
-  // eventually gonna' have things like callbacks and whatnot
+	OutputStyle  int
+	IncludePaths []string
+	ImagePath    string
+	// eventually gonna' have things like callbacks and whatnot
 }
 
 type FileContext struct {
-  Options
-  InputPath    string
-  OutputString string
-  ErrorStatus  int
-  ErrorMessage string
+	Options
+	InputPath    string
+	OutputString string
+	ErrorStatus  int
+	ErrorMessage string
 }
 
 // Constants/enums for the output style.
 const (
-  NESTED_STYLE = iota
-  EXPANDED_STYLE
-  COMPACT_STYLE
-  COMPRESSED_STYLE
+	NESTED_STYLE = iota
+	EXPANDED_STYLE
+	COMPACT_STYLE
+	COMPRESSED_STYLE
 )
 
 func CompileFile(goCtx *FileContext) {
-  // set up the underlying C context struct
-  cCtx := C.sass_new_file_context()
-  cCtx.input_path            = C.CString(goCtx.InputPath)
-  defer C.free(unsafe.Pointer(cCtx.input_path))
-  cCtx.options.output_style  = C.int(goCtx.Options.OutputStyle)
-  cCtx.options.include_paths = C.CString(strings.Join(goCtx.Options.IncludePaths, ":"))
-  defer C.free(unsafe.Pointer(cCtx.options.include_paths))
-  cCtx.options.image_path    = C.CString(goCtx.Options.ImagePath)
-  defer C.free(unsafe.Pointer(cCtx.options.image_path))
-  // call the underlying C compile function to populate the C context
-  C.sass_compile_file(cCtx)
-  // extract values from the C context to populate the Go context object
-  goCtx.OutputString = C.GoString(cCtx.output_string)
-  goCtx.ErrorStatus  = int(cCtx.error_status)
-  goCtx.ErrorMessage = C.GoString(cCtx.error_message)
-  // don't forget to free the C context!
-  C.sass_free_file_context(cCtx)
+	// set up the underlying C context struct
+	cCtx := C.sass_new_file_context()
+	cCtx.input_path = C.CString(goCtx.InputPath)
+	defer C.free(unsafe.Pointer(cCtx.input_path))
+	cCtx.options.output_style = C.int(goCtx.Options.OutputStyle)
+	cCtx.options.include_paths = C.CString(strings.Join(goCtx.Options.IncludePaths, ":"))
+	defer C.free(unsafe.Pointer(cCtx.options.include_paths))
+	cCtx.options.image_path = C.CString(goCtx.Options.ImagePath)
+	defer C.free(unsafe.Pointer(cCtx.options.image_path))
+	// call the underlying C compile function to populate the C context
+	C.sass_compile_file(cCtx)
+	// extract values from the C context to populate the Go context object
+	goCtx.OutputString = C.GoString(cCtx.output_string)
+	goCtx.ErrorStatus = int(cCtx.error_status)
+	goCtx.ErrorMessage = C.GoString(cCtx.error_message)
+	// don't forget to free the C context!
+	C.sass_free_file_context(cCtx)
 }
