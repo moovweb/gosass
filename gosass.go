4ac992da7f901e3be97872c58ae700adbdef123e
Tim Wojtaszek <tim.wojtaszek@moovweb.com>
2015-01-27 11:12:05 -0800

    Merge Feature Branch go1.4.2

--- gosass.go
+++ gosass.go
@@ -1,8 +1,8 @@
 package gosass
 
 /*
-#cgo LDFLAGS: -L../../clibs/lib -lsass -lstdc++
-#cgo CFLAGS: -I../../clibs/include
+#cgo LDFLAGS: -L../../../../../clibs/lib -lsass -lstdc++
+#cgo CFLAGS: -I../../../../../clibs/include
 
 #include <stdlib.h>
 #include <sass_interface.h>
@@ -14,10 +14,10 @@ import (
 )
 
 type Options struct {
-	OutputStyle  int
+	OutputStyle    int
 	SourceComments bool
-	IncludePaths []string
-	ImagePath    string
+	IncludePaths   []string
+	ImagePath      string
 	// eventually gonna' have things like callbacks and whatnot
 }
 
