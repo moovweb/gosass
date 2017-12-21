01eb6751283b9c079cba032019c5327dc383191f
Joshua Delsman <voxxit@users.noreply.github.com>
2017-12-21 13:36:32 -0800

    fix

--- gosass.go
+++ gosass.go
@@ -1,8 +1,8 @@
 package gosass
 
 /*
-#cgo LDFLAGS: -L{CLIBS_HOME}/lib -lsass -lstdc++
-#cgo CFLAGS: -I{CLIBS_HOME}/include
+#cgo LDFLAGS: -L${CLIBS_HOME}/lib -lsass -lstdc++
+#cgo CFLAGS: -I${CLIBS_HOME}/include
 
 #include <stdlib.h>
 #include <sass_interface.h>
